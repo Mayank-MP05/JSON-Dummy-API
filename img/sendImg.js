595fd2837cb302a1103efaa6ef2ab583fc37e4c9
Mayank_MP5 <mayank5pande@gmail.com>
2020-08-18 14:42:02 +0530

    Image Sending Dropped

--- img/sendImg.js
+++ img/sendImg.js
@@ -10,4 +10,25 @@ app.get("/getImg", (req, res) => {
   });
 });
 
+app.get("/getImg/:side", (req, res) => {
+  res.sendFile(`${__dirname}/dummy.png`, {}, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Sent");
+    }
+  });
+  console.log(req.params);
+});
+
+app.get("/getImg/:width/:height", (req, res) => {
+  res.sendFile(`${__dirname}/dummy.png`, {}, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Sent");
+    }
+  });
+  console.log(req.params);
+});
 app.startDummyServer();
