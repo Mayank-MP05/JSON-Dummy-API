9881b2fe2003476e0ed22a1e2dd22445dd6fb930
Mayank_MP5 <mayank5pande@gmail.com>
2020-08-18 08:09:41 +0530

    Successfully genrated QandA Database with Node

--- defaultRoutes/generateQandAData.js
+++ defaultRoutes/generateQandAData.js
@@ -1,12 +1,19 @@
 const fs = require("fs");
-for (i = 0; i < 11; i++) {
+const chance = require("chance").Chance();
+
+const arr = [];
+for (i = 0; i < 50; i++) {
   // jsonFile.writeFile('QandA.database.json', "id :" + i + " square :" + i * i);
-  fs.writeFile(
-    "QandA.database.json",
-    JSON.stringify({ msg: "This si First JSON Wirte" }),
-    "utf8",
-    () => {
-      console.log("JSON Writing Done");
-    }
-  );
+  const obj = {
+    QuestionID: i + 1,
+    question: `${chance.sentence()} ?`,
+    answer: chance.paragraph(),
+    upvotes: Math.ceil(Math.random() * 101),
+    downvotes: Math.ceil(Math.random() * 101),
+  };
+  arr.push(obj);
 }
+
+fs.writeFile("QandA.database.json", JSON.stringify(arr), "utf8", () => {
+  console.log("JSON Writing Done");
+});
