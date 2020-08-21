4ef2c80cd280bf9231af1dddd1d252b58f77a719
Mayank_MP5 <mayank5pande@gmail.com>
2020-08-21 17:18:33 +0530

    Default Routes Users All Routes Working ...

--- defaultRoutes/Users.route.js
+++ defaultRoutes/Users.route.js
@@ -7,16 +7,66 @@ module.exports = (app) => {
 
   app.get("/users/:id", (req, res) => {
     const id = parseInt(req.params.id);
-    if (id > 0 && id < 51) {
-      const requiredUser = usersDB.filter(
-        (singleUser) => singleUser.UserID === id
-      );
+    const requiredUser = usersDB.filter(
+      (singleUser) => singleUser.UserID === id
+    );
+    //console.log(requiredQuestion);
+    res.send(requiredUser);
+  });
+
+  //Update Single User
+  app.put("/users/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+
+    let obj = req.body;
+    let updateIndex = usersDB.findIndex((singleUser) => {
+      return singleUser.UserID === id;
+    });
+    if (updateIndex !== -1) {
+      usersDB[updateIndex] = {
+        UserID: id,
+        ...obj,
+      };
+
       //console.log(requiredQuestion);
-      res.send(requiredUser);
+      res.json({
+        UserID: id,
+        ...obj,
+      });
     } else {
-      res.send({
-        msg: "Student Index Out of Range",
+      res.status(404).json({
+        err: `No User with ID : ${id}`,
       });
     }
+  });
+
+  //Post to  User | adding new User
+  app.post("/users", (req, res) => {
+    const length = usersDB.length;
+    const id = usersDB[length - 1].UserID
+      ? usersDB[length - 1].UserID + 1
+      : 1;
+    let obj = req.body;
+    usersDB.push({
+      UserID: id,
+      ...obj,
+    });
+    //console.log(requiredQuestion);
+    res.json({
+      UserID: id,
+      ...obj,
+    });
+  });
+
+  //Deleting product by ID
+  app.delete("/users/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const deleted = usersDB.filter(
+      (singleUser) => singleUser.UserID === id
+    );
+    usersDB = usersDB.filter(
+      (singleUser) => singleUser.UserID !== id
+    );
+    res.json(deleted);
   });
 };
